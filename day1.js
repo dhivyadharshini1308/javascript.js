72cedcf0213092f94dca12a35b96727786fa4d2d
dhivyadharshini1308 <dhivyadharshinivelu@gmail.com>
2025-07-20 20:26:49 +0530

    Create day1.js

---
+++ day1.js
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="index.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
+var numvber=12;
+var name=`js`;
+var iscloudy=true;
+
+console.log(number+" "+name+" "+iscloudy);
+document.write(number+" "+name+" "+iscloudy);
+ //task2
+ console.log(typeof(num));
+ console.log(typeof(name));
+ console.log(typeof(iscloudy));
+ //task3
+ var num1=parseInt(prompt("enter the value"));
+ var num2=parseInt(prompt("enter the value"));
+ console.log(num1+num2);
+ console.log(num1*num2);
+ console.log(num1-num2);
+ console.log(num1/num2);
